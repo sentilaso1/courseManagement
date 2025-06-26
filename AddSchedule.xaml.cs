590f22fb649e82aee4799b76cdd757b8666701a3
Pham Tung Duong <phamtungduong763@gmail.com>
2025-06-26 09:04:25 +0700

    Initial commit

---
+++ AddSchedule.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CourseManagerment.Models;
+
+namespace CourseManagerment
+{
+    /// <summary>
+    /// Interaction logic for AddSchedule.xaml
+    /// </summary>
+    public partial class AddSchedule : Window
+    {
+        ApContext context = new ApContext();
+        public CourseSchedule newSchedule {  get;private set; }
+        public AddSchedule(int? selectedCourseId = null)
+        {
+
+            InitializeComponent();
+            if (selectedCourseId != null)
+            {
+                cbCourseId.SelectedValue = selectedCourseId;
+                cbCourseId.IsEnabled=false;
+            }
+            LoadcbData();
+        }
+        private void LoadcbData()
+        {
+            cbCourseId.ItemsSource = context.Courses.ToList();
+            cbRoomId.ItemsSource= context.Rooms.ToList();
+        }
+
+        private void saveAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                newSchedule = new CourseSchedule
+                {
+                    CourseId = (int)cbCourseId.SelectedValue,
+                    TeachingDate = Picker.SelectedDate??DateTime.Now,
+                    Slot = int.Parse(Slot.Text),
+                    RoomId = (int)cbRoomId.SelectedValue,
+                    Description = Description.Text,
+                };
+                DialogResult = true;
+                Close();
+
+            }catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CourseManagerment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseManagerment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApContext context = new ApContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadCourse();
+            LoadData();
+        }
+        private void LoadCourse()
+        {
+            var courses = context.Courses.Include(c => c.Subject).ToList();
+            cbCourse.ItemsSource = courses;
+            cbCourse.DisplayMemberPath = "CourseCode";
+            cbCourse.SelectedValuePath = "CourseId";
+        }
+        private void LoadData(int? courseId=null)
+        {
+            var result = new List<CourseSchedule>();
+            if (courseId == null)
+            {
+                result = context.CourseSchedules.ToList();
+            }
+            else
+            {
+                result = context.CourseSchedules.Where(sc => sc.CourseId == courseId).ToList();
+            }
+           
+            dgSchedule.ItemsSource = result;
+        }
+
+        private void cbCourse_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbCourse.SelectedValue is int courseId) { 
+                LoadData(courseId);
+            }
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            cbCourse.SelectedIndex = -1;
+            LoadData();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            int courseId = (int)cbCourse.SelectedValue;
+            var addSchedule = new AddSchedule(courseId);
+
+            if (addSchedule.ShowDialog() == true) {
+                context.CourseSchedules.Add(addSchedule.newSchedule);
+                context.SaveChanges();
+                LoadData(courseId);
+            }
+
+        }
+    }
+}
